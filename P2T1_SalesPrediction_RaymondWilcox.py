1c7be8f61ed73f0d8e43fb43d18f226a0635b9c1
wilcoxr6132 <40347974+wilcoxr6132@users.noreply.github.com>
2018-09-25 20:51:02 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_RaymondWilcox.py
@@ -0,0 +1,13 @@
+# SalesPredictionProgram
+# CTI-110 P2T1-Sales Prediction
+# 09/25/2018
+# Raymond Wilcox
+
+# what are the total sale
+TotalSales = float(input('enter projected sales: '))
+
+# calculate the profit as 23 percent of total sales
+profit = TotalSales * 0.23
+
+# display the profit
+print ('The profit is $', format(profit, ',.2f'))
